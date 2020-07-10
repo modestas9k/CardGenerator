a0eb86cae5ce9532869e5649e10cec0444fe7f40
Modestas Skirius <modestas@pop-os.localdomain>
2020-07-10 23:46:59 +0300

    nice

--- script.js
+++ script.js
@@ -4,56 +4,85 @@ const img = document.getElementById("pic");
 const country = document.getElementById("country");
 const email = document.getElementById("email");
 const phone = document.getElementById("phone");
+const gender = document.getElementsByName("gender");
 
 document.forms[0].addEventListener("submit", (event) => {
     event.preventDefault();
-
+    console.log(gender);
     cards.innerHTML = "";
     const nameNumber = Number(event.target.elements.number.value);
 
-    for (let i = 0; i < nameNumber; i++) {
-        fetch("https://randomuser.me/api/")
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data.results[0]);
+    let url = "https://randomuser.me/api/?results=" + nameNumber + "&inc=name,gender";
+
+    if (img.checked === true) {
+        url = url + ",picture";
+    }
+
+    if (country.checked === true) {
+        url = url + ",location";
+    }
+
+    if (email.checked === true) {
+        url = url + ",email";
+    }
+    if (phone.checked === true) {
+        url = url + ",cell";
+    }
+    if (getRadioValue(gender) === "female") {
+        url = url + "&gender=female";
+    } else if (getRadioValue(gender) === "male") {
+        url = url + "&gender=male";
+    }
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data.results);
+
+            for (let i = 0; i < nameNumber; i++) {
                 const card = document.createElement("div");
                 card.id = "card";
 
                 const fullName = document.createElement("h3");
-                fullName.textContent =
-                    data.results[0].name.first +
-                    " " +
-                    data.results[0].name.last;
+                fullName.textContent = data.results[i].name.first + " " + data.results[i].name.last;
 
                 if (img.checked === true) {
                     let img = document.createElement("img");
-                    img.src = data.results[0].picture.large;
+                    img.src = data.results[i].picture.large;
                     card.append(img);
                 }
                 card.append(fullName);
 
                 if (country.checked === true) {
                     const country = document.createElement("h4");
-                    country.textContent = data.results[0].location.country;
+                    country.textContent = data.results[i].location.country;
                     card.append(country);
                 }
                 if (email.checked === true) {
                     const email = document.createElement("h5");
-                    email.textContent = data.results[0].email;
+                    email.textContent = data.results[i].email;
                     card.append(email);
                 }
                 if (phone.checked === true) {
                     const phone = document.createElement("h5");
-                    phone.textContent = data.results[0].cell;
+                    phone.textContent = data.results[i].cell;
                     card.append(phone);
                 }
-                if (data.results[0].gender == "male") {
+                if (data.results[i].gender == "male") {
                     card.style.background = "#C3F3EC";
                 } else {
                     card.style.background = "#F3C3CA";
                 }
 
                 cards.append(card);
-            });
-    }
+            }
+        });
 });
+function getRadioValue(radioList) {
+    let result = "";
+    for (let i = 0; i < radioList.length; i++) {
+        if (radioList[i].checked === true) {
+            result = radioList[i].value;
+        }
+    }
+    return result;
+}
